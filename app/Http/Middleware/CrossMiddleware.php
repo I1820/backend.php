0e2a8078f0286fab43a4244b3227411d8d01dcfe
Sajjad <sajjad.rahnama7@gmail.com>
2017-12-22 15:53:15 +0330

    Cross

--- app/Http/Middleware/CrossMiddleware.php
+++ app/Http/Middleware/CrossMiddleware.php
@@ -18,7 +18,7 @@ class CrossMiddleware
         if(env('CLIENT_ORIGIN'))
             return $next($request)
                 ->header('Access-Control-Allow-Origin', env('CLIENT_ORIGIN'))
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
                 ->header('Access-Control-Allow-Headers',$request->header('Access-Control-Request-Headers'));
         return $next($request);
     }
