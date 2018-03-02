f6313f899e64d2b48cb69c643bcd6581e571fda5
Sajjad <sajjad.rahnama7@gmail.com>
2018-03-02 17:27:25 +0330

    Config

--- config/iot.php
+++ config/iot.php
@@ -5,7 +5,7 @@ return [
         'serverBaseUrl' => env('LORA_BASE_URL','https://platform.ceit.aut.ac.ir:50013'),
         'organizationID' => '1',
         'networkServerID' => '1',
-        'serviceProfileID' => '2dc02c2b-2be9-4bef-ade4-f3408e62a08c',
+        'serviceProfileID' => env('SERVICE_PROFILE_ID','2dc02c2b-2be9-4bef-ade4-f3408e62a08c'),
     ],
     'core' => [
         'serverBaseUrl' => env('CORE_BASE_URL','172.23.132.50'),
