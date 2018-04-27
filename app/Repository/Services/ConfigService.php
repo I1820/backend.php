ef9b0e07ad69716022dd44d4584dd46aee82ea85
Sajjad <sajjad.rahnama7@gmail.com>
2018-04-27 23:18:18 +0430

    Widget Delete

--- app/Repository/Services/ConfigService.php
+++ app/Repository/Services/ConfigService.php
@@ -36,7 +36,9 @@ class ConfigService
         if (!$config)
             $config = UserConfig::create(['user_id' => Auth::user()['_id']]);
         $config_widgets = isset($config['widgets']) ? $config['widgets'] : [];
-        $thing = Auth::user()->things()->where('dev_eui', $data->get('devEUI'))->first();
+        $thing = Auth::user()->things()->with(['thing' => function ($query) use ($data) {
+            $query->where('dev_eui', $data->get('devEUI'));
+        }])->first();
         if (!$thing)
             throw new GeneralException('شی یافت نشد', GeneralException::NOT_FOUND);
         $widget = $data->only([
