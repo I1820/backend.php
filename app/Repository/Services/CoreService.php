c7bc92915ac7078476f75ca29d3bbd7d547463a8
Sajjad <sajjad.rahnama7@gmail.com>
2018-06-01 14:18:44 +0430

    Encode Decode Bug Core Service

--- app/Repository/Services/CoreService.php
+++ app/Repository/Services/CoreService.php
@@ -117,10 +117,10 @@ class CoreService
      * @return array
      * @throws GeneralException
      */
-    public function encode(Project $project, Thing %thing, $data)
+    public function encode(Project $project, Thing $thing, $data)
     {
         $url = '/api/encode/' . $thing['interface']['devEUI'];
-        $response = $this->send($url, $data, 'post', $project['container']['runner']['port']));
+        $response = $this->send($url, $data, 'post', $project['container']['runner']['port']);
         return $response; 
     }
  
@@ -131,10 +131,10 @@ class CoreService
      * @return array
      * @throws GeneralException
      */
-    public function decode(Project $project, Thing %thing, $data)
+    public function decode(Project $project, Thing $thing, $data)
     {
         $url = '/api/decode/' . $thing['interface']['devEUI'];
-        $response = $this->send($url, $data, 'post', $project['container']['runner']['port']));
+        $response = $this->send($url, $data, 'post', $project['container']['runner']['port']);
         return $response; 
     }
 
