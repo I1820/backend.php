93f4e6ae7df10e30979bbf049013636a4c3bf7d5
skings <samanf74@gmail.com>
2018-06-03 17:49:00 +0430

    ignore some code

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -21,8 +21,8 @@ class TestController extends Controller
         $lan = new LanService($curl);
 
 //        return $lan->postDeviceProfile($request->get('name')); // postprofiledevice
-//        return $lan->postDevice(collect($request->all()),"5b13c9026abc2ad22c052e8c", "5b13d90570d071dcb1aa8a22"); //post device
-        return $lan->getDevices("5b13c9026abc2ad22c052e8c"); // list devices
+        return $lan->postDevice(collect($request->all()),"5b13c9026abc2ad22c052e8c", "5b13d90570d071dcb1aa8a22"); //post device
+//        return $lan->getDevices("5b13c9026abc2ad22c052e8c"); // list devices
 //        return $lan->deleteDevice("5b13c9026abc2ad22c052e8c",12); // delete
 
     }
