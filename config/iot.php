dfbe184bea2b52e299129e44fe10ac8851480323
Parham Alvani <parham.alvani@gmail.com>
2018-06-06 03:22:23 +0430

    Changes PM port name.

--- config/iot.php
+++ config/iot.php
@@ -15,7 +15,7 @@ return [
     ],
     'core' => [
         'serverBaseUrl' => env('CORE_BASE_URL', '172.23.132.50'),
-        'port' => '8080',
+        'pmPort' => '8080',
         'dmPort' => '1372',
         'downLinkPort' => '1373',
         'gmPort' => '1374'
