891212f098bb81e41a195108da2b98b731738edd
Sajjad <sajjad.rahnama7@gmail.com>
2018-07-14 03:56:26 +0430

    Captcha

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -104,8 +104,8 @@ return [
             'rule-name' => 'custom-message',
         ],
         'g-recaptcha-response' => [
-            'required' => 'Please verify that you are not a robot.',
-            'captcha' => 'Captcha error! try again later or contact site admin.',
+            'required' => 'روی گزینه من رباط نیستم کلیک کنید.',
+            'captcha' => 'بعدا تلاش کنید.',
         ],
     ],
 
