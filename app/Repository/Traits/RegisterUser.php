4f6ad022f9a1c21e30ad0b78acaab48172b16531
amirosein <amirhosein.abbasi594@gmail.com>
2018-12-19 18:56:29 +0330

    Password Validation

--- app/Repository/Traits/RegisterUser.php
+++ app/Repository/Traits/RegisterUser.php
@@ -58,14 +58,15 @@ trait RegisterUser
             'email.unique' => 'این ایمیل قبلا ثبت شده است',
             'password.required' => 'لطفا رمزعبور را وارد کنید',
             'password.min' => 'رمز عبور حداقل باید ۶ کارکتر باشد',
+            'password.regex'=>'رمز عبور باید شامل حداقل یک حرف بزرگ، یک حرف کوچک و یک کاراکتر خاص باشد'
         ];
 
         $validator = Validator::make($request->all(), [
             'name' => 'string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:6|confirmed|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])$/',
         ], $messages);
-
+        
         if ($validator->fails())
             throw new GeneralException($validator->errors()->first(), GeneralException::VALIDATION_ERROR);
         return;
