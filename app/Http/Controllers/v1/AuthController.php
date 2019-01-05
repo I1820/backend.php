2046cb96a67ab98d872b37f81cb138cb5cba5931
amirosein <amirhosein.abbasi594@gmail.com>
2019-01-05 16:23:56 +0330

    User Login Update

--- app/Http/Controllers/v1/AuthController.php
+++ app/Http/Controllers/v1/AuthController.php
@@ -68,8 +68,10 @@ class AuthController extends Controller
         
         $token = $this->userService->generateToken($request);
         $user = Auth::user();
-        $user['last_login_IP']=request()->ip();
-        $user['last_login_time']=new DateTime();
+        $user()->update(['last_login_IP' => request()->ip() ]) ;
+        $user()->update(['last_login_time' => new DateTime() ]) ;
+        $user['last_login_IP']= request()->ip();
+        $user['last_login_time']= new DateTime();
         if (!$user->active)
             throw new AuthException(AuthException::M_USER_NOT_ACTIVE, AuthException::UNAUTHORIZED);
         $config = ['portainer_url' => env('PORTAINER_URL'), 'prometheus_url' => env('PROMETHEUS_URL')];
