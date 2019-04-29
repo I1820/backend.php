2c7ef2a33fce13a57053badc49bc155a3bab266c
Parham Alvani <parham.alvani@gmail.com>
2019-04-30 01:38:52 +0430

    Let's add database seeder to have a better project.

---
+++ database/seeds/PermissionsSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::collection('permissions')->delete();
+        DB::collection('permissions')->insert([
+            "slug" => "CREATE-PROJECT", "name" => "ساخت پروژه"
+        ]);
+        DB::collection('permissions')->insert([
+            "slug" => "EDIT-PROJECT", "name" => "ویرایش پروژه"
+        ]);
+        DB::collection('permissions')->insert([
+            "slug" => "CREATE-THING", "name" => "ساخت شی"
+        ]);
+        DB::collection('permissions')->insert([
+            "slug" => "EDIT-THING", "name" => "ویرایش شی"
+        ]);
+        DB::collection('permissions')->insert([
+            "slug" => "EDIT-THING", "name" => "ویرایش شی"
+        ]);
+        DB::collection('permissions')->insert([
+            "slug" => "DELETE-THING", "name" => "حذف شی"
+        ]);
+        DB::collection('permissions')->insert([
+            "slug" => "DELETE-PROJECT", "name" => "حذف پروژه"
+        ]);
+    }
+}
