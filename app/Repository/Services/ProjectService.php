a5de766999b9508e99ce2a5ee42a08151a8947f6
Parham Alvani <parham.alvani@gmail.com>
2019-06-02 01:30:29 +0430

    Revert "Projects does not have lora by default."
    This reverts commit c1bc11a259299432d932eaa804f763dc90851edc.

--- app/Repository/Services/ProjectService.php
+++ app/Repository/Services/ProjectService.php
@@ -60,21 +60,17 @@ class ProjectService
     public function insertProject(Request $request)
     {
         $id = new ObjectId();
-        $container = $this->coreService->createProject($id); // create project with `pm`
+        $application_id = $this->loraService->postApp($request->get('description'), $id);
+        $container = $this->coreService->createProject($id);
         $project = Project::create([
             '_id' => $id,
             'name' => $request->get('name'),
             'description' => $request->get('description'),
             'active' => true,
             'container' => $container,
+            'application_id' => $application_id
         ]);
         return $project;
-    }
-
-    public function enableLoraOnProject(Request $request, Project $project)
-    {
-        $project->application_id = $this->loraService->postApp($request->get('description'), $project->_id);
-        $project->save();
     }
 
     /**
