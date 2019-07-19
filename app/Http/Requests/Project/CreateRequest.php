830e6e01a124791c4919ccc8d83e9c6080c639ec
Parham Alvani <parham.alvani@gmail.com>
2019-07-20 02:14:15 +0430

    feat: Add Project Creation and Update Requests

---
+++ app/Http/Requests/Project/CreateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Project;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string'
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'لطفا نام پروژه را وارد کنید',
+            'description.required' => 'لطفا توضیحات را وارد کنید'
+        ];
+    }
+}
---
+++ app/Http/Requests/Project/UpdateRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Project;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'string|max:255',
+            'description' => 'string'
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+        ];
+    }
+}
