034e4ad6034d7dee7b51f0cced35bfe6efeb5a06
Parham Alvani <parham.alvani@gmail.com>
2019-07-19 15:13:28 +0000

    feat: Add General Information about Project

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="I1820 API Backend",
+ *      description="The Glue",
+ *      @OA\Contact(
+ *          name="Parham Alvani",
+ *          email="parham.alvani@gmail.com"
+ *      ),
+ *      @OA\License(
+ *         name="GPLv3",
+ *         url="https://www.gnu.org/licenses/gpl-3.0.html"
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
