41b31bdaa51345ca1139cef5a7f1e585087cbb7b
Parham Alvani <parham.alvani@gmail.com>
2019-07-20 03:16:20 +0000

    chore: Remove Redundant Field

--- app/Http/Controllers/v1/ProjectController.php
+++ app/Http/Controllers/v1/ProjectController.php
@@ -24,7 +24,6 @@ class ProjectController extends Controller
 {
     protected $projectService;
     protected $coreService;
-    protected $loraService;
     protected $thingService;
     protected $pmService;
 
